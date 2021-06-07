4d5758dff1c20730b542fd19e782bdd6cf556566
Theearya <75364609+Theearya@users.noreply.github.com>
2021-06-07 15:44:38 +0530

    added

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode3 = "FUNCTION";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill('white')
+text('B D A T A S E A',700,150)
+fill('lightblue')
+text('HINT: Stores All Information',700,170)
+fill('white')
+text('T N F U C O N I',100,255)
+fill('lightblue')
+text('HINT: Performs A Particular Task',100,275)
+
+}
